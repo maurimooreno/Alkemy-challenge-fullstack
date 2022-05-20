73bdc1d8784bc3abf293b33dd8cfd6ef0a97b3a9
maurimooreno <maurimooreno@gmail.com>
2022-05-20 17:30:57 -0300

    servidor corriendo, conexcion a la db funcionando

---
+++ back/libs/sequelize.js
@@ -0,0 +1,25 @@
+const { Sequelize } = require('sequelize');
+const { config } = require('../config/config');
+const setupModels = require('../db/models/index')
+const USER = encodeURIComponent(config.dbUser);
+const PASSWORD = encodeURIComponent(config.dbPassword);
+
+const URI = `postgres://${USER}:${PASSWORD}@${config.dbHost}:${config.dbPort}/${config.dbName}` /* local */
+
+const sequelize = new Sequelize(URI, {
+  dialect: 'postgres',
+  logging: false
+});
+
+setupModels(sequelize);
+
+const {} = sequelize.models;
+
+
+sequelize.sync({ force: false })
+  .then(() => {
+    console.log(`created/updated database`);  
+  })
+  .catch(err => console.log(err));
+
+module.exports = sequelize
---
+++ back/routes/index.js
@@ -0,0 +1,6 @@
+
+function routerApi (app) {
+  // app.use();
+}
+
+module.exports = routerApi;
